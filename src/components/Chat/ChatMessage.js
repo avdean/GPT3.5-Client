68c73d0ee000b9987c17572ddee364154609f3ec
Avdean <34350184+avdean@users.noreply.github.com>
2023-03-12 11:08:32 +0100

    Added syntax highlighting to message

--- src/components/Chat/ChatMessage.js
+++ src/components/Chat/ChatMessage.js
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from "react";
-import {motion, AnimatePresence} from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { vsDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const ChatMessage = (props, index) => {
   const messageRef = useRef(null);
@@ -12,24 +14,9 @@ const ChatMessage = (props, index) => {
     }
   }, [messageRef]);
 
-  if (props.sender === "ChatGPT") {
-    return (
-      <div className="chat-message-gpt">
-        <AnimatePresence>
-          <motion.div
-            ref={messageRef}
-            key={index}
-            className="messageChatgpt"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 3 }}
-          >
-            {props.message}
-          </motion.div>
-        </AnimatePresence>
-      </div>
-    );
-  } else
+  const isFromChatGPT = props.sender === "ChatGPT";
+
+  if (!isFromChatGPT) {
     return (
       <div className="chat-message">
         <AnimatePresence>
@@ -46,6 +33,70 @@ const ChatMessage = (props, index) => {
         </AnimatePresence>
       </div>
     );
+  }
+
+  // Regular expression to match code blocks
+  const regex = /(`{3})([\s\S]*?)(`{3})/g;
+
+  // Check if the message contains code or not
+  const isCode = regex.test(props.message);
+
+  // If the message does not contain code, render it as plain text
+  if (!isCode) {
+    return (
+      <div className="chat-message-gpt">
+        <AnimatePresence>
+          <motion.div
+            ref={messageRef}
+            key={index}
+            className="messageChatgpt"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 3 }}
+          >
+            {props.message}
+          </motion.div>
+        </AnimatePresence>
+      </div>
+    );
+  }
+
+  // If the message contains code, split it into parts
+  const parts = props.message.split(regex);
+
+  // Render each part as plain text or code block
+  return (
+    <div className="chat-message-gpt">
+      <AnimatePresence>
+        <motion.div
+          ref={messageRef}
+          key={index}
+          className="messageChatgpt"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 3 }}
+        >
+          {parts.map((part, i) => {
+            if (regex.test(part)) {
+              // If part is code block, render it inside SyntaxHighlighter component
+              return (
+                <SyntaxHighlighter
+                  language="javascript"
+                  style={vsDark}
+                  key={i}
+                >
+                  {part.substring(3, part.length - 3)}
+                </SyntaxHighlighter>
+              );
+            } else {
+              // If part is not code block, render it as plain text
+              return <span key={i}>{part}</span>;
+            }
+          })}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
 };
 
 export default ChatMessage;

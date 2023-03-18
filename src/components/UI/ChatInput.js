98682e99ec3f178877b46a4c5924bbc1a6356a8f
Avdean <34350184+avdean@users.noreply.github.com>
2023-03-18 01:14:08 +0100

    Return

--- src/components/UI/ChatInput.js
+++ src/components/UI/ChatInput.js
@@ -1,27 +1,23 @@
-import { useState, useRef } from "react"; 
 import UseAnimations from "react-useanimations";
 import TextareaAutosize from "react-textarea-autosize";
 import loading from "react-useanimations/lib/loading";
-import { FaRegPaperPlane } from "react-icons/fa"; 
-import { motion } from "framer-motion"; 
-
-const ChatInput = (props) => { 
-
-const [inputValue, setInputValue] = useState(""); 
-const [loading, setLoading] = useState(false); 
-const timeoutRef = useRef(null); 
-
-function handleInputChange(e) { const value = e.target.value; clearTimeout(timeoutRef.current); timeoutRef.current = setTimeout(() => { setInputValue(value); }, 500); } 
-
-function handleSend(e) { e.preventDefault(); setLoading(true); props.send(inputValue).finally(() => { setLoading(false); setInputValue(""); }); } 
-
-function handleEnterPress(e) { if (e.keyCode === 13 && !e.shiftKey) { e.preventDefault(); handleSend(e); } }
-
+import { FaRegPaperPlane } from "react-icons/fa";
+import { motion } from "framer-motion";
+ 
+const ChatInput = (props) => {
+ 
+  function enterPress(e) {
+    if (e.keyCode === 13 && e.shiftKey === false) {
+      e.preventDefault();
+      props.handleSend();
+    }
+ 
   return (
     <motion.div className="chat-input-holder">
-      <form className="inputForm" onSubmit={props.handleSend} onKeyDown={handleEnterPress}>
+      <form className="inputForm" onSubmit={props.handleSend} onKeyDown={enterPress}>
         <TextareaAutosize
-          className="chat-input-textarea noScroll"
+          class
+# Name="chat-input-textarea noScroll"
           maxRows="6"
           value={props.input}
           onChange={(e) => props.setInput(e.target.value)}
@@ -44,5 +40,6 @@ function handleEnterPress(e) { if (e.keyCode === 13 && !e.shiftKey) { e.preventD
     </motion.div>
   );
 };
-
+ 
 export default ChatInput;
+ 

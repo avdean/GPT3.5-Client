cf28e68fbebeb3bae3aadc4351c844a75a264e66
Avdean <34350184+avdean@users.noreply.github.com>
2023-11-29 09:12:40 +0100

    Div Test

--- src/components/UI/SideMenu.js
+++ src/components/UI/SideMenu.js
@@ -36,6 +36,11 @@ const SideMenu = (props) => {
               <option value="gpt-4">GPT-4</option>
               <option value="gpt-4-1106-preview">GPT-4.5 Turbo</option>
             </select>
+            <div className="dropDown" onChange={handleModelChange} value={props.currentModel}>
+              <div value="gpt-3.5-turbo">GPT-3.5 Turbo</div>
+              <div value="gpt-4">GPT-4</div>
+              <div value="gpt-4-1106-preview">GPT-4.5 Turbo</div>
+            </div>
         </div>
             
         <div className="sidemenuInner">
